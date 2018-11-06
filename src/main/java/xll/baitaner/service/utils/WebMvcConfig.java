5356f6a9157dfb810a23c184abaed55ff74059c8
xiell0725 <xiell0725@thundersoft.com>
2018-11-06 19:00:36 +0800

    swagger修改提交

--- src/main/java/xll/baitaner/service/utils/WebMvcConfig.java
+++ src/main/java/xll/baitaner/service/utils/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package xll.baitaner.service.utils;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -14,10 +15,15 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger.html")
+        registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
     }
 }
