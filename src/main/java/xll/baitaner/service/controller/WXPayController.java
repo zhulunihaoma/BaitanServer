5f294aa5156e9d341c19a006de103768ecf0358f
XLL <744428439@qq.com>
2019-06-19 23:34:13 +0800

    支付模块重构初步提交

--- src/main/java/xll/baitaner/service/controller/WXPayController.java
+++ src/main/java/xll/baitaner/service/controller/WXPayController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xll.baitaner.service.entity.Order;
 import xll.baitaner.service.service.OrderService;
+import xll.baitaner.service.service.PayService;
 import xll.baitaner.service.utils.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -38,11 +39,8 @@ public class WXPayController {
 
     private String TAG = "Baitaner-WXPayController";
 
-    /**
-     * 是否在正式环境
-     */
-    @Value("${baitaner.runtime}")
-    private boolean runtimeOrDev;
+    @Autowired
+    private PayService payService;
 
     /**
      * 发起微信支付
@@ -50,86 +48,9 @@ public class WXPayController {
      * @param openId
      * @return
      */
-    @GetMapping("order/pay")
+    @GetMapping("wxpay")
     public ResponseResult pay(String orderId, String openId){
-        Order order = orderService.getOrder(orderId);
-        if(order.getState() != 0){
-            return ResponseResult.result(1, "订单并不是未支付订单",null);
-        }
-
-        if(!order.getOpenId().equals(openId)){
-            return ResponseResult.result(1, "订单用户和当前发起支付用户不匹配",null);
-        }
-
-        try{
-            config = WXPayConfigImpl.getInstance();
-            wxPay = new WXPay(config);
-
-            //支付金额
-            DecimalFormat decimalFormat = new DecimalFormat("0");
-            String total_fee = decimalFormat.format((order.getTotalMoney() * 100));
-
-            //商户订单号
-            String out_trade_no = order.getOrderId();
-
-            String notify_url = runtimeOrDev ? "https://www.eastzebra.cn/service/wxpay/notify" :
-                                               "https://www.eastzebra.cn/service_test/wxpay/notify";
-
-            HashMap<String, String> data = new HashMap<>();
-            data.put("body","小仙女烘焙-支付" + total_fee);//商品描述
-            data.put("out_trade_no",out_trade_no);//商户订单号
-            data.put("fee_type", "CNY");//货币
-            data.put("total_fee", total_fee);//标价金额 单位为分 不能有小数点 todo 测试用1分钱
-            data.put("spbill_create_ip", "192.168.1.1");//终端IP
-            data.put("notify_url", notify_url);//异步接收微信支付结果通知的回调地址,外网可访问地址
-            data.put("trade_type", "JSAPI");//交易类型 JSAP:公众号支付
-            data.put("openid", openId);//用户标识,微信用户ID 暂时使用我的openid
-
-            System.out.print("WXPay unifiedOrder data: " + data.toString());
-
-            //返回预支付订单信息，发送给前端
-            Map<String, String> result = wxPay.unifiedOrder(data);
-            System.out.print("WXPay unifiedOrder result: " + result.toString());
-            if(result.get("return_code").equals("FAIL")){
-                return ResponseResult.result(1, result.get("return_msg"),null);
-            }
-
-            String prepay_id = result.get("prepay_id");//预支付交易会话标识
-            //出错跳转错误页面
-            if(prepay_id.equals("")){
-                return ResponseResult.result(1, "预支付单生成错误",null);
-            }
-
-            String appId = result.get("appid");//小程序appid
-            String timeStamp = String.valueOf(System.currentTimeMillis() / 1000);//时间戳
-            String nonceStr = WXPayUtil.generateNonceStr();//随机串
-            String packageValue = "prepay_id=" + prepay_id;
-            String signType = "MD5";
-
-            //生成信息签名
-            HashMap<String, String> finalpackage = new HashMap<>();
-            finalpackage.put("appId", appId);
-            finalpackage.put("timeStamp", timeStamp);
-            finalpackage.put("nonceStr", nonceStr);
-            finalpackage.put("package", packageValue);
-            finalpackage.put("signType", signType);
-            String paySign = WXPayUtil.generateSignature(finalpackage, config.getKey());
-
-            //直接打包JSON格式发送到前端
-            JSONObject json = new JSONObject();
-            json.put("appId", appId);
-            json.put("timeStamp", timeStamp);
-            json.put("nonceStr", nonceStr);
-            json.put("package", packageValue);
-            json.put("signType", signType);
-            json.put("paySign", paySign);
-
-            return ResponseResult.result(0, "成功",json);
-
-        } catch (Exception e){
-            e.printStackTrace();
-            return ResponseResult.result(1, "统一支付接口获取预支付订单出错",null);
-        }
+        return payService.payMent(orderId, openId, 0);
     }
 
     /**
@@ -208,134 +129,16 @@ public class WXPayController {
     }
 
 
-    /***************好近钱方支付测试******************/
+    /***************好近钱方支付******************/
     /**
-     * 好近钱方支付测试
+     * 好近钱方支付
      * @param fee
      * @param openId
      * @return
      */
     @GetMapping("qfpay")
-    public ResponseResult qfpay(String fee, String openId){
-
-        String payUrl = "https://openapi.qfpay.com/trade/v1/payment";
-        String out_trade_no = SerialUtils.getSerialId();
-        String txdtm = DateUtils.getCurrentDate();
-
-        String code = "382368FBDDE84259AA9029D38D2BC5CA";
-        String key = "0910E30FD78648BE913F167F9594932E";
-        String mchid = "M4V9GiYLdj";
-
-        try{
-            //发起支付数据
-            HashMap<String, String> data = new HashMap<>();
-            data.put("txamt",fee); //订单支付金额，单位分
-            data.put("txcurrcd", "CNY");//货币
-            data.put("pay_type", "800213");//支付类型    微信小程序支付:800213
-            data.put("out_trade_no", out_trade_no);//外部订单号
-            data.put("txdtm", txdtm);//请求交易时间  格式为：YYYY-MM-MM HH:MM:SS
-            data.put("sub_openid", openId);//微信小程序的openid
-            data.put("goods_name", "qfTest");//商品名称标示
-            data.put("mchid", mchid);
-
-            //MD5签名
-            String sign = WXPayUtil.MD5("goods_name=qfTest&mchid=" + mchid + "&out_trade_no=" + out_trade_no +
-                    "&pay_type=800213&sub_openid="+openId+"&txamt=1&txcurrcd=CNY&txdtm="+txdtm + key).toUpperCase();
-            LogUtils.info(TAG, "发起支付的data: " + data + "\n 签名sign: " + sign + "\n sing2: " + sign);
-
-            String UTF8 = "UTF-8";
-            URL httpUrl = new URL(payUrl);
-            HttpURLConnection httpURLConnection = (HttpURLConnection) httpUrl.openConnection();
-            httpURLConnection.setDoOutput(true);
-            httpURLConnection.setDoInput(true);
-            httpURLConnection.setRequestMethod("POST");
-            httpURLConnection.setUseCaches(false);
-
-            //http头添加验证属性
-            httpURLConnection.setRequestProperty("X-QF-APPCODE", code);
-            httpURLConnection.setRequestProperty("X-QF-SIGN", sign);
-
-            httpURLConnection.connect();
-
-            JSONObject jsonObject = JSONObject.fromObject(data);
-            LogUtils.info(TAG, "qfpay reqBody：" + jsonObject.toString());
-
-            OutputStream outputStream = httpURLConnection.getOutputStream();
-            // 注意编码格式
-            outputStream.write(jsonObject.toString().getBytes(UTF8));
-            outputStream.close();
-
-            //获取返回内容
-            InputStream inputStream = httpURLConnection.getInputStream();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, UTF8));
-            final StringBuffer stringBuffer = new StringBuffer();
-            String line = null;
-            while ((line = bufferedReader.readLine()) != null) {
-                stringBuffer.append(line);
-            }
-
-            String resp = stringBuffer.toString();
-            if (stringBuffer!=null) {
-                try {
-                    bufferedReader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (inputStream!=null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            //获取数据发送前端
-            LogUtils.info(TAG, "支付返回 resp: " + resp);
-            JSONObject payObj = (JSONObject)JSONObject.fromObject(resp).get("pay_params");
-            LogUtils.info(TAG, "获取返回数据中的pay_params: " + payObj);
-
-            if(payObj == null){
-                return ResponseResult.result(1, "钱方返回数据出错",resp);
-            }
-
-            //生成信息签名
-            HashMap<String, String> finalpackage = new HashMap<>();
-            finalpackage.put("appId", payObj.get("appId").toString());
-            finalpackage.put("timeStamp", payObj.get("timeStamp").toString());
-            finalpackage.put("nonceStr", payObj.get("nonceStr").toString());
-            finalpackage.put("package", payObj.get("package").toString());
-            finalpackage.put("signType", payObj.get("signType").toString());
-            String paySign = WXPayUtil.generateSignature(finalpackage, key);
-
-            //直接打包JSON格式发送到前端
-            payObj.put("paySign", paySign);
-
-            LogUtils.info(TAG, "发送给前端小程序JSON: " + payObj);
-            return ResponseResult.result(0, "成功",payObj);
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            return ResponseResult.result(1, "支付接口出错",null);
-        }
-    }
-
-
-    /**
-     * 好近钱方支付测试,使用工具类
-     * @param fee
-     * @param openId
-     * @return
-     */
-    @GetMapping("qfwxpay")
-    public ResponseResult qfwxpay(String fee, String openId){
-
-        String out_trade_no = SerialUtils.getSerialId();
-        String txdtm = DateUtils.getCurrentDate();
-
-        JSONObject payObj = QfWxPay.QfPayMent(fee, out_trade_no, txdtm, openId);
-        LogUtils.info(TAG, "qfwxpay 返回数据中的pay_params: " + payObj);
-        return ResponseResult.result(0, "成功",payObj);
+    public ResponseResult qfwxpay(String orderId, String openId){
+        return payService.payMent(orderId, openId, 1);
     }
 
 
---
+++ src/main/java/xll/baitaner/service/service/PayService.java
@@ -0,0 +1,135 @@
+package xll.baitaner.service.service;
+
+import com.github.wxpay.sdk.WXPay;
+import com.github.wxpay.sdk.WXPayUtil;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import xll.baitaner.service.entity.Order;
+import xll.baitaner.service.utils.*;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 类名：PayService
+ * 描述：支付类接口服务，包括微信支付、钱方支付，体现等
+ * 创建者：xie
+ * 日期：2019.6.19
+ **/
+public class PayService {
+
+    private String TAG = "Baitaner-PayService";
+
+    @Autowired
+    private OrderService orderService;
+
+    private WXPayConfigImpl config; //微信支付配置文件
+    private WXPay wxPay;
+
+
+    public ResponseResult payMent(String orderId, String openId, int payType) {
+        Order order = orderService.getOrder(orderId);
+
+        if (order.getState() != 0) {
+            return ResponseResult.result(1, "订单不是未支付订单", null);
+        }
+        if (!order.getOpenId().equals(openId)) {
+            return ResponseResult.result(1, "订单用户和当前发起支付用户不匹配", null);
+        }
+
+        try {
+            //支付金额
+            DecimalFormat decimalFormat = new DecimalFormat("0");
+            String total_fee = decimalFormat.format((order.getTotalMoney() * 100));
+            //todo 测试用1分钱
+            total_fee = "1";
+
+            //商户订单号
+            String out_trade_no = order.getOrderId();
+
+            if (payType == 0) {
+                //微信支付
+                return WXPayMent(total_fee, out_trade_no, openId);
+            } else if (payType == 1) {
+                //钱方支付
+                String txdtm = DateUtils.getCurrentDate();
+                JSONObject payObj = QfWxPay.QfPayMent(total_fee, out_trade_no, txdtm, openId);
+                LogUtils.info(TAG, "qfwxpay 返回数据中的pay_params: " + payObj);
+                return ResponseResult.result(1, "success", payObj);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    String notify_url = "https://www.eastzebra.cn/service/wxpay/notify";
+
+    private ResponseResult WXPayMent(String total_fee, String out_trade_no, String openId) {
+
+        try {
+
+            if (config == null || wxPay == null) {
+                config = WXPayConfigImpl.getInstance();
+                wxPay = new WXPay(config);
+            }
+
+            HashMap<String, String> data = new HashMap<>();
+            data.put("body", "小仙女烘焙-支付" + total_fee);//商品描述
+            data.put("out_trade_no", out_trade_no);//商户订单号
+            data.put("fee_type", "CNY");//货币
+            data.put("total_fee", total_fee);//标价金额 单位为分 不能有小数点
+            data.put("spbill_create_ip", "192.168.1.1");//终端IP
+            data.put("notify_url", notify_url);//异步接收微信支付结果通知的回调地址,外网可访问地址
+            data.put("trade_type", "JSAPI");//交易类型 JSAP:公众号支付
+            data.put("openid", openId);//用户标识,微信用户ID 暂时使用我的openid
+
+            LogUtils.debug(TAG, "WXPay unifiedOrder data: " + data.toString());
+
+            //返回预支付订单信息，发送给前端
+            Map<String, String> result = wxPay.unifiedOrder(data);
+            System.out.print("WXPay unifiedOrder result: " + result.toString());
+            if (result.get("return_code").equals("FAIL")) {
+                return ResponseResult.result(1, result.get("return_msg"), null);
+            }
+
+            String prepay_id = result.get("prepay_id");//预支付交易会话标识
+            //出错跳转错误页面
+            if (prepay_id.equals("")) {
+                return ResponseResult.result(1, "预支付单生成错误,prepay_id为空", null);
+            }
+
+            String appId = result.get("appid");//小程序appid
+            String timeStamp = String.valueOf(System.currentTimeMillis() / 1000);//时间戳
+            String nonceStr = WXPayUtil.generateNonceStr();//随机串
+            String packageValue = "prepay_id=" + prepay_id;
+            String signType = "MD5";
+
+            //生成信息签名
+            HashMap<String, String> finalpackage = new HashMap<>();
+            finalpackage.put("appId", appId);
+            finalpackage.put("timeStamp", timeStamp);
+            finalpackage.put("nonceStr", nonceStr);
+            finalpackage.put("package", packageValue);
+            finalpackage.put("signType", signType);
+            String paySign = WXPayUtil.generateSignature(finalpackage, config.getKey());
+
+            //直接打包JSON格式发送到前端
+            JSONObject json = new JSONObject();
+            json.put("appId", appId);
+            json.put("timeStamp", timeStamp);
+            json.put("nonceStr", nonceStr);
+            json.put("package", packageValue);
+            json.put("signType", signType);
+            json.put("paySign", paySign);
+
+            return ResponseResult.result(0, "success", json);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseResult.result(1, "统一支付接口获取预支付订单出错", null);
+        }
+    }
+}
