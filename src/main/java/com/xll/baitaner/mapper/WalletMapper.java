5c58fdaec6b61e9cc248936456f69fede275c6e9
dengyy <dengyanty@163.com>
2019-07-01 22:28:22 +0800

    修改钱包返回字段

--- src/main/java/com/xll/baitaner/mapper/WalletMapper.java
+++ src/main/java/com/xll/baitaner/mapper/WalletMapper.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Repository
 public interface WalletMapper {
 
-    final String walletFields = "id,shop_id,order_id,user_id,amount,operator,create_data";
+    final String walletFields = "id,shop_id,order_id,open_id,amount,operator,create_data";
 
     /**
      * 根据日期查询店铺的提现记录
@@ -27,7 +27,7 @@ public interface WalletMapper {
     @Select("select amount from `shop_wallet` where shop_id=#{shopId} and operator='DEC' and DATE_FORMAT(create_data,'%Y-%m-%d')=#{dateStr} LIMIT #{page.offset},#{page.size}")
     List<String> getWalletWithAmountByDate(@Param("shopId") Integer shop, @Param("dateStr") String dateStr, @Param("page") Pageable page);
 
-    @Insert("insert into `shop_wallet` (shop_id,order_id,user_id,amount,operator) " +
+    @Insert("insert into `shop_wallet` (shop_id,order_id,open_id,amount,operator) " +
             "values (#{wallet.shopId},#{wallet.orderId},#{wallet.userId},#{wallet.amount},#{wallet.operator})")
     int insertWalletRecord(@Param("wallet") ShopWallet wallet);
 
@@ -37,6 +37,6 @@ public interface WalletMapper {
      * @param shopId
      * @return
      */
-    @Select("select " + walletFields + "from `shop_wallet` where shop_id=#{shopId}")
+    @Select("select " + walletFields + " from `shop_wallet` where shop_id=#{shopId}")
     List<ShopWallet> selectAllByShopId(@Param("shopId") Integer shopId);
 }
