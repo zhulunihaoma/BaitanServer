77d56312166222d00d499f18d3d7e71d10ff27ec
dengyanyan <835942599@qq.com>
2019-07-02 10:53:54 +0800

    修改打包配置

--- src/main/java/com/xll/baitaner/Application.java
+++ src/main/java/com/xll/baitaner/Application.java
@@ -3,12 +3,19 @@ package com.xll.baitaner;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @MapperScan("com.xll.baitaner.mapper")
-@PropertySource(value={"application.properties","WEB-INF/classes/application.properties"},ignoreResourceNotFound = true)
-public class Application {
+@PropertySource(value = {"application.properties", "WEB-INF/classes/application.properties"}, ignoreResourceNotFound = true)
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
