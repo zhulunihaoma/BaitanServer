8ee3772b0ef9b9b150297be3b23c656a3c89d769
dengyanyan <835942599@qq.com>
2019-08-26 16:24:23 +0800

    增加图片压缩接口

--- src/main/java/com/xll/baitaner/controller/PicUploadController.java
+++ src/main/java/com/xll/baitaner/controller/PicUploadController.java
@@ -4,18 +4,24 @@ import com.xll.baitaner.service.PicUploadService;
 import com.xll.baitaner.utils.ResponseResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * 描述：图片上传controller
  * 创建者：xie
  * 日期：2017/9/27
  **/
+@Slf4j
 @Api(value = "图片上传controller")
 @RestController
 public class PicUploadController {
@@ -42,5 +48,32 @@ public class PicUploadController {
         //压缩图片
         picUploadService.zipPicLoad(res);
         return ResponseResult.result(res != null ? 0 : 1, res != null ? "successs" : "fail", res);
+    }
+
+    /**
+     * 图片压缩
+     *
+     * @return
+     */
+    @PostMapping("/zipservicepic")
+    public ResponseResult zipPic() {
+        String oldpath = ClassUtils.getDefaultClassLoader().getResource("").getPath()
+                + "../../../../webapps/servicepicture/";
+        String newpath = ClassUtils.getDefaultClassLoader().getResource("").getPath()
+                + "../../../../webapps/zippicture/";
+        File oldFile = new File(oldpath);
+        File[] files = oldFile.listFiles();
+        for (File file : files) {
+            String oldName = oldpath.concat(file.getName());
+            String newName = newpath.concat(file.getName());
+            try {
+                Thumbnails.of(oldName).scale(1f).outputQuality(0.25f).toFile(newName);
+            } catch (IOException e) {
+                log.error("图片转换压缩错误,name:{},e:{}", file.getName(), e);
+                e.printStackTrace();
+            }
+        }
+        return ResponseResult.result(0, "success", 1);
+
     }
 }
