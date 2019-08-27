863e8664d386d63197abb728c1eb319177fd0165
dengyy <dengyanty@163.com>
2019-08-28 07:42:11 +0800

    增加快递单接口

--- src/main/java/com/xll/baitaner/controller/ExpressController.java
+++ src/main/java/com/xll/baitaner/controller/ExpressController.java
@@ -1,9 +1,14 @@
 package com.xll.baitaner.controller;
 
 import com.xll.baitaner.entity.ExpressInfo;
+import com.xll.baitaner.service.ExpressInfoService;
 import com.xll.baitaner.utils.ResponseResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author dengyy
@@ -12,8 +17,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ExpressController {
 
+    @Resource
+    ExpressInfoService expressInfoService;
+
     @PutMapping("express/add")
     public ResponseResult addOne(ExpressInfo express) {
-        return ResponseResult.result(0, "success", "");
+        try {
+            int ex = expressInfoService.addExpress(express);
+            return ResponseResult.result(0, "success", ex);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseResult.result(1, "fail", e);
+        }
+    }
+
+    @GetMapping("express/ids")
+    public ResponseResult getExpressId(Integer shopId, Integer id) {
+        try {
+            List<Integer> ids = expressInfoService.queryExpressIds(shopId, id);
+            return ResponseResult.result(0, "success", ids);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseResult.result(1, "fail", e);
+        }
     }
 }
